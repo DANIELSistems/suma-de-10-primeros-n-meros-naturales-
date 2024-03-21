dad74743eacdcf2e237ccb5caaef8ab0548ed001
DANIELSistems <danielvasquez@colegioguanenta.edu.co>
2024-03-21 17:37:55 -0500

    se realizo el programa

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # suma-de-10-primeros-n-meros-naturales-
 programa para sumar los 10 primeros números enteros
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
+
---
+++ sumanumerosenteros.py
@@ -0,0 +1,20 @@
+import math
+
+#input
+
+N=int(input("Digite el valor de N: "))
+
+#processing
+
+suma = 0
+
+i = 1
+
+while (i<=N):
+    suma=suma + i
+    i=i + 1
+
+#output
+print("La suma es: " + str(suma))
+
+#FIN
